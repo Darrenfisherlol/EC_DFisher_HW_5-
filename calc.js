7a55af66db3e2036166f614d29e0b1dd2b8ca2f2
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-10-26 14:20:11 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,19 @@
+
+
+
+let x = myFunction(4, 3);   
+
+
+
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mult(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
